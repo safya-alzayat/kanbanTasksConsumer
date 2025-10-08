7834e10e08d0db7fab3b1aeed27202147dde9c79
Safya Alzayat <safya.alzayat@tngtech.com>
2025-10-08 12:09:42 +0200

    first commit

---
+++ src/main/java/com/KanbanTasks/kanbanTasksConsumer/KanbanTasksConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.KanbanTasks.kanbanTasksConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanTasksConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanTasksConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/KanbanTasks/kanbanTasksConsumer/KanbanTasksConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KanbanTasks.kanbanTasksConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanTasksConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
